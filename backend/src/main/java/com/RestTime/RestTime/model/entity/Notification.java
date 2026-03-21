ec71aadf711b594eb0ad5be02b923c79933c3500
amhine <nihadamhine@gmail.com>
2026-03-21 23:58:01 +0000

    feat: create notification entity to track rh/employee alerts

---
+++ backend/src/main/java/com/RestTime/RestTime/model/entity/Notification.java
@@ -0,0 +1,33 @@
+package com.RestTime.RestTime.model.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import com.RestTime.RestTime.model.enumeration.TypeNotification;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "notification")
+@Builder
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titre;
+    private String message;
+    private LocalDateTime dateEnvoi;
+
+    @Builder.Default
+    private boolean lue = false;
+
+    @Enumerated(EnumType.STRING)
+    private TypeNotification type;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
