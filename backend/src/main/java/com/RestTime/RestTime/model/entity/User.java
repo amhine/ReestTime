6ea450371e506bf955f9c67ecb4c1a0bdb58216f
amhine <nihadamhine@gmail.com>
2026-03-21 23:49:07 +0000

    feat: implement user entity with basic fields (username, password, email)

---
+++ backend/src/main/java/com/RestTime/RestTime/model/entity/User.java
@@ -0,0 +1,50 @@
+package com.RestTime.RestTime.model.entity;
+import jakarta.persistence.*;
+import lombok.*;
+import com.RestTime.RestTime.model.enumeration.Role;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String prenom;
+    @Column(unique = true)
+    private String email;
+    private String motDePasse;
+    private Double soldeConges;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private String telephone;
+    private String adresse;
+    private String photoProfile;
+    private String poste;
+    private String departement;
+
+
+
+    @OneToMany(mappedBy = "user")
+    private List<DemandeConge> demandes;
+
+    @OneToMany(mappedBy = "user")
+    private List<Notification> notifications;
+
+    @Column(name = "reset_token")
+    private String resetToken;
+
+    @Column(name = "reset_token_creation_date")
+    private LocalDateTime resetTokenCreationDate;
+}
