f3ddd949bb6dfdbcb7023f3a4f5a96c608fe3797
amhine <nihadamhine@gmail.com>
2026-03-23 00:43:30 +0100

    feat: implement email service with smtp for leave notifications

--- backend/src/main/java/com/RestTime/RestTime/service/impl/EmailServiceImpl.java
+++ backend/src/main/java/com/RestTime/RestTime/service/impl/EmailServiceImpl.java
@@ -25,7 +25,6 @@ public class EmailServiceImpl implements EmailService {
                 "Vous avez demandé la réinitialisation de votre mot de passe pour votre compte RestTime.\n" +
                 "Veuillez cliquer sur le lien ci-dessous pour définir un nouveau mot de passe :\n\n" +
                 resetLink + "\n\n" +
-                "Ce lien est valable pendant 24 heures.\n" +
                 "Si vous n'êtes pas à l'origine de cette demande, vous pouvez ignorer cet e-mail en toute sécurité.\n\n" +
                 "Cordialement,\n" +
                 "L'équipe RestTime.");
