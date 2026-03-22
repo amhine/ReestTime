16023dac45c6b094fd4abf4ef018d5bc33a757ac
amhine <nihadamhine@gmail.com>
2026-03-22 00:05:03 +0000

    feat: add change password dto for profile security update

---
+++ backend/src/main/java/com/RestTime/RestTime/dto/ChangePasswordRequestDTO.java
@@ -0,0 +1,15 @@
+package com.RestTime.RestTime.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordRequestDTO {
+    private String ancienMotDePasse;
+    private String nouveauMotDePasse;
+}
