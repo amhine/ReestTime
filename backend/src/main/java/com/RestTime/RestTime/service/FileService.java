7d5f4707f5779f8553f8f0f5b1a7abfca4b5f7f5
amhine <nihadamhine@gmail.com>
2026-03-22 01:24:48 +0000

    refactor: update FileService

--- backend/src/main/java/com/RestTime/RestTime/service/FileService.java
+++ backend/src/main/java/com/RestTime/RestTime/service/FileService.java
@@ -1,4 +1,11 @@
 package com.RestTime.RestTime.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
 public interface FileService {
+    String saveFile(MultipartFile file, String subDir) throws IOException;
+    void deleteFile(String fileName);
+
 }
