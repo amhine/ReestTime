57b0faea6f29727ec5194ca87d815eebe7e7103e
amhine <nihadamhine@gmail.com>
2026-03-22 00:05:57 +0000

    feat: add announcement entity for company-wide messages

---
+++ backend/src/main/java/com/RestTime/RestTime/model/entity/Announcement.java
@@ -0,0 +1,25 @@
+package com.RestTime.RestTime.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "announcements")
+public class Announcement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titre;
+
+    @Column(columnDefinition = "TEXT")
+    private String contenu;
+
+    private LocalDateTime dateCreation;
+}
