e76ca16d5ef9b2d15e5c35d3fd5e0b85722980bf
amhine <nihadamhine@gmail.com>
2026-03-23 01:11:00 +0100

    chore: add github actions ci pipeline for automated build and tests

--- backend/src/main/java/GenerateHash.java
+++ backend/src/main/java/GenerateHash.java
@@ -3,7 +3,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class GenerateHash {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String password = "rh@rh.com";
+        String password = "test@rh.com";
         String hash = encoder.encode(password);
         System.out.println("Password: " + password);
         System.out.println("BCrypt Hash: " + hash);
